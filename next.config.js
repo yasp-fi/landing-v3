4f523c7f39ac06f8f0fc4aea0f8ad6df1109a9ad
Danil Sergeev <33134604+danil-sergeev@users.noreply.github.com>
2023-09-25 11:12:48 +0100

    chore: add .turbo to ignore (#2)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["@yasp/interface-kit", "@yasp/models"],
+  transpilePackages: ["@yasp/interface-kit"],
   output: "standalone",
   experimental: {
     outputFileTracingRoot: path.join(__dirname, "../../")
