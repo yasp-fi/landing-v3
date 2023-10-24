db3cbf518dd10ff1f88ed0a6d7b78a0386619bb8
Danil Sergeev <33134604+danil-sergeev@users.noreply.github.com>
2023-10-24 10:54:08 +0100

    feat: add FB pixel (#5)
    Co-authored-by: danil-sergeev <cz.gorudo@gmail.com>

--- app/head.tsx
+++ app/head.tsx
@@ -1,5 +1,6 @@
 import { BASE_APP_URL, DESCRIPTION, TITLE } from '../lib/constants';
 import React from 'react';
+import Script from "next/script";
 
 export default function Head() {
   return (
@@ -25,6 +26,23 @@ export default function Head() {
       <meta name="twitter:description" content={DESCRIPTION} />
       <meta name="twitter:image" content={`${BASE_APP_URL}/yasp-thumb.png`} />
       <meta name="twitter:image:alt" content={DESCRIPTION} />
+      <Script>
+        {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1075475973898383');
+        fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript><img height="1" width="1" style="display:none"
+                     src="https://www.facebook.com/tr?id=1075475973898383&ev=PageView&noscript=1"
+      /></noscript>
     </>
   );
 }
